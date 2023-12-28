a45c3ac8803580ce3be11d9d90369035136e84ba
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2023-12-28 13:07:18 -0500

    updated php stuff

--- php/send.php
+++ php/send.php
@@ -1,8 +1,47 @@
 <?php
-echo "test";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../lib/PHPMailer/src/Exception.php';
+require '../lib/PHPMailer/src/PHPMailer.php';
+require '../lib/PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$mail -> isSMTP();
+$mail-> Host="smtp.gmail.com";
+$mail-> SMTPAuth = true;
+
+$mail->Username="gradysitefeedbackform@gmail.com";
+$mail->Password="ewjmhzxeznyxqgys";
+
+$mail->SMTPSecure = "tls";
+$mail->Port=587;
+
+$mail->setFrom("gradysitefeedbackform@gmail.com");
+
+$mail->addAddress("olivarezmig@gmail.com");
+
+$mail->isHTML(true);
+
+$mail->Subject="Test Email Using PHPMailer";
+
+$mail->Body = "This is plain text email body";
+
+$mail->SMTPDebug=2;
+
+$mail->Send();
+
+if ($mail->Send()){
+    echo "test";
 echo '
 <script>
 alert("Sent Successfully");
 document.location.href = `/workSite/#/feedback`;
 </script>';
+} else {
+    echo "Error...! ";
+}
+$mail->smtpClose();
 ?>
