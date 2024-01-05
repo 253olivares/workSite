83438a25f88465edf0199a2e8015beb446a22ad9
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-01-05 15:38:29 -0500

    test

--- php/send.php
+++ php/send.php
@@ -9,39 +9,43 @@ require '../lib/PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
-$mail -> isSMTP();
-$mail-> Host="smtp.gmail.com";
-$mail-> SMTPAuth = true;
+// $mail -> isSMTP();
+// $mail-> Host="smtp.gmail.com";
+// $mail-> SMTPAuth = true;
 
-$mail->Username="gradysitefeedbackform@gmail.com";
-$mail->Password="ewjmhzxeznyxqgys";
+// $mail->Username="gradysitefeedbackform@gmail.com";
+// $mail->Password="ewjmhzxeznyxqgys";
 
-$mail->SMTPSecure = "tls";
-$mail->Port=587;
+// $mail->SMTPSecure = "tls";
+// $mail->Port=587;
 
-$mail->setFrom("gradysitefeedbackform@gmail.com");
+// $mail->setFrom("gradysitefeedbackform@gmail.com");
 
-$mail->addAddress("olivarezmig@gmail.com");
+// $mail->addAddress("olivarezmig@gmail.com");
 
-$mail->isHTML(true);
+// $mail->isHTML(true);
 
-$mail->Subject="Test Email Using PHPMailer";
+// $mail->Subject="Test Email Using PHPMailer";
 
-$mail->Body = "This is plain text email body";
+// $mail->Body = "This is plain text email body";
 
-$mail->SMTPDebug=2;
+// $mail->SMTPDebug=2;
 
-$mail->Send();
+// $mail->Send();
 
-if ($mail->Send()){
-    echo "test";
+// if ($mail->Send()){
+// echo '
+// <script>
+// alert("Sent Successfully");
+// document.location.href = `/workSite/#/feedback`;
+// </script>';
+// } else {
+//     echo "Error...! ";
+// }
+// $mail->smtpClose();
 echo '
 <script>
 alert("Sent Successfully");
 document.location.href = `/workSite/#/feedback`;
 </script>';
-} else {
-    echo "Error...! ";
-}
-$mail->smtpClose();
 ?>
